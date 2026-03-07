cf24df1b7dcd7a1d235b7393b262c9c33f0dacf2
Hasan Hijjo <131011994+hasanhijjo@users.noreply.github.com>
2026-03-07 15:38:53 +0200

    Refactor login fields and button in LoginPage class

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,15 +1,18 @@
 class LoginPage:
+   
     def __init__(self, page):
         self.page = page
-
-        self._username_field = page.get_by_placeholder("Username")
-        self._password_field = page.get_by_placeholder("Password")
-        self._login_button = page.get_by_role("button", name="Login")
+        self._user_field = page.get_by_placeholder("Username")
+        self._pass_field = page.get_by_placeholder("Password")
+        self._btn = page.get_by_role("button", name="Login")
 
     def navigate(self):
+      
         self.page.goto("https://opensource-demo.orangehrmlive.com/")
 
     def login(self, user, pwd):
-        self._username_field.fill(user)
-        self._password_field.fill(pwd)
-        self._login_button.click()
+        
+        self._user_field.fill(user)
+        self._pass_field.fill(pwd)
+        self._btn.click()
+        
